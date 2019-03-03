b3a55c6c826538fa59827a439d60ba9249a18817
kipkorir cornelius <mishrakiprop@gmail.com>
2019-03-03 09:23:50 +0300

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,11 @@ var rolldice=function(){
     return Math.floor(Math.floor.random())+1;
 
 }
-function participant(turn){
+function Participant(turn){
     this.rolldice=0;
     this.dicescore=0;
     this.totalscore=0;
-    
+    this.turn=turn;
+    this.nameofPlayer=nameofPlayer;
 }
+Participant.prototype.rollonce
