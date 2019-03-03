d3a535b623b643d020ef0306bdaaf3995879771c
kipkorir cornelius <mishrakiprop@gmail.com>
2019-03-03 09:14:57 +0300

    ad functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 var firstplayer;
 var secondplayer;
 
+var rolldice=function(){
+    return Math.floor(Math.floor.random())+1;
+
+}
+function participant(turn){
+    this.rolldice=0;
+    this.dicescore=0;
+    this.totalscore=0;
+    
+}
